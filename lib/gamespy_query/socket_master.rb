2ec243768e74251f10ec97081b292d805c8ffe10
Sickboy <sb@dev-heaven.net>
2012-02-23 14:53:52 +0100

    fbm

--- lib/gamespy_query/socket_master.rb
+++ lib/gamespy_query/socket_master.rb
@@ -5,26 +5,23 @@ module GamespyQuery
   class SocketMaster < Socket
     def initialize addrs
       @addrs = addrs
+      @id_packet = ID_PACKET
+      @packet = CHALLENGE_PACKET + @id_packet
+
+      @timeout = 5 # Per select iteration
     end
 
+    # States:
+    # 0 - Not begun
+    # 1 - Sent Challenge
+    # 2 - Received Challenge
+    # 3 - Sent Challenge Response
+    # 4 - Receive Data
+    # 5 - Ready
     def process!
       jar = {}
-      # TODO: Keep filling the sockets array when sockets.size  < max_connections
-      max_connections = 32
+      max_connections = 100
       max_connections_int = max_connections - 1
-      timeout = 10
-
-      id_packet = ID_PACKET
-      packet = CHALLENGE_PACKET + id_packet
-
-      # States:
-      # 0 - Not begun
-      # 1 - Sent Challenge
-      # 2 - Received Challenge
-      # 3 - Sent Challenge Response
-      # 4 - Receive Data
-      # 5 - Ready
-
 
       until @addrs.empty?
         addrs = @addrs[0..max_connections_int]
@@ -33,7 +30,20 @@ module GamespyQuery
         sockets.each_with_index {|s, i| jar[s] = {addr: addrs[i], data: [], state: 0, stamp: nil, needs_challenge: false, max_packets: MAX_PACKETS, failed: false}}
 
         until sockets.empty?
-          #puts "Loop, #{sockets.size}"
+          read_sockets = []
+          write_sockets = []
+
+          sockets.each {|s| unless jar[s][:state] >= 5; [0, 2].include?(jar[s][:state]) ? write_sockets << s : read_sockets << s; end }
+          #puts "Read: #{read_sockets.inspect}, Write: #{write_sockets.inspect}"
+          unless ready = IO.select(read_sockets, write_sockets, nil, @timeout)
+            puts "Timeout, no usable sockets within timeout"
+            sockets.each{|s| s.close unless s.closed?}
+            sockets = []
+            next
+          end
+          puts "Loop, Sockets: #{sockets.size}, AddrsLeft: #{@addrs.size}, ReadReady: #{"#{ready[0].size}, WriteReady: #{ready[1].size}, ExcReady: #{ready[2].size}" unless ready.nil?}"
+
+=begin
           if sockets.size < max_connections
             count = (max_connections - sockets.size) - 1
             addrs = @addrs[0..count]
@@ -44,127 +54,156 @@ module GamespyQuery
 
             sockets += socks
           end
-
-          ready = IO.select(sockets, sockets, sockets)
-
-          processed = []
+=end
 
           # Read
           ready[0].each do |s|
             entry = jar[s]
-
-            begin
-              data = s.recvfrom(RECEIVE_SIZE)
-            rescue => e
-              puts "Error: #{e.message}, #{entry}"
-              entry[:failed] = true
-              sockets.delete s
-              s.close
-              next
-            end
-            #puts "Read: #{s.inspect}, #{entry}: #{data}"
-            processed << s
-            entry[:stamp] = Time.now
-
-            case entry[:state]
-              when 1
-                puts "Read (1): #{entry}"
-                # Receive challenge
-                challenge = data[0]
-                str = get_string(challenge)
-                puts "Received challenge response (#{str.length}): #{str.inspect}"
-                need_challenge = !(str.sub(STR_X0, STR_EMPTY) =~ RX_NO_CHALLENGE)
-                if need_challenge
-                  Tools.debug {"Needs challenge!"}
-                  str = str.sub(RX_CHALLENGE, STR_EMPTY).gsub(RX_CHALLENGE2, STR_EMPTY).to_i
-                  challenge_packet = sprintf(STR_BLA, handle_chr(str >> 24), handle_chr(str >> 16), handle_chr(str >> 8), handle_chr(str >> 0))
-                  entry[:needs_challenge] = challenge_packet
-                end
-
-                entry[:state] = 2
-              when 3, 4
-                puts "Read (3,4): #{entry}"
-                entry[:state] = 4
-
-                index = 0
-
-                game_data = get_string(data[0])
-                Tools.debug {"Received (#{entry[:data].size + 1}):\n\n#{game_data.inspect}\n\n#{game_data}\n\n"}
-
-                if game_data.sub(STR_GARBAGE, STR_EMPTY)[RX_SPLITNUM]
-                  splitnum = $1
-                  flag = splitnum.unpack("C")[0]
-                  index = (flag & 127).to_i
-                  last = flag & 0x80 > 0
-                  # Data could be received out of order, use the "index" id when "last" flag is true, to determine total packet_count
-                  entry[:max_packets] = index + 1 if last # update the max
-                  puts "Splitnum: #{splitnum.inspect} (#{splitnum}) (#{flag}, #{index}, #{last}) Max: #{entry[:max_packets]}"
-                else
-                  entry[:max_packets] = 1
-                end
-                entry[:data][index] = game_data #.sub(RX_X0_S, STR_EMPTY) # Cut off first \x00 from package
-
-                if entry[:data].size >= entry[:max_packets] # OR we received the end-packet and all packets required
-                  entry[:state] = 5
-                  s.close
-                  sockets.delete s
-                end
-            end
+            handle_read s, entry, sockets
           end
 
           # Write
-          (ready[1] - processed).each do |s|
+          ready[1].each do |s|
             entry = jar[s]
-            #puts "Write: #{s.inspect}, #{entry}"
-            processed << s
-            begin
-              case entry[:state]
-                when 0
-                  puts "Write (0): #{entry}"
-                  # Send Challenge
-                  s.puts packet
-                  entry[:state] = 1
-                  entry[:stamp] = Time.now
-                when 2
-                  puts "Write (2): #{entry}"
-                  # Send Challenge response
-                  packet = entry[:needs_challenge] ? BASE_PACKET + id_packet + entry[:needs_challenge] + FULL_INFO_PACKET_MP : BASE_PACKET + id_packet + FULL_INFO_PACKET_MP
-                  s.puts packet
-                  entry[:state] = 3
-              end
-            rescue => e
-              puts "Error: #{e.message}, #{entry}"
-              entry[:failed] = true
-              sockets.delete s
-              s.close
-              next
-            end
-
-            if Time.now - entry[:stamp] > timeout
-              puts "TimedOut: #{entry}"
-              entry[:failed] = true
-              sockets.delete s
-              s.close
-            end
+            handle_write s, entry, sockets
           end
 
           # Exceptions
-          ready[2].each do |s|
-            puts "Exception: #{s.inspect}"
-            s.close
-            sockets.delete s
-            entry = jar[s]
-            entry[:failed] = true
-          end
+          #ready[2].each do |s|
+          #  handle_exc s, entry, sockets
+          #end
         end
       end
       puts "Finished"
+
+      cool, dude = 0, 0
       jar.each_pair do |k, v|
+        if v[:state] > 1
+          cool += 1
+        else
+          dude += 1
+        end
         puts v.inspect
       end
+      puts "Success: #{cool}, Failed: #{dude}"
 
 
       return jar
+    end
+
+    def handle_read s, entry, sockets
+      case entry[:state]
+        when 1
+          begin
+            #data = s.recvfrom(RECEIVE_SIZE)
+            data = s.recvfrom_nonblock(RECEIVE_SIZE)
+              #sockets.delete s
+              #s.close
+          rescue => e
+            puts "Error: #{e.message}, #{entry}"
+            entry[:failed] = true
+            sockets.delete s
+            s.close
+            return
+          end
+          puts "Read (1): #{s.inspect}, #{entry}: #{data}"
+          entry[:stamp] = Time.now
+
+          # Receive challenge
+          challenge = data[0]
+          str = get_string(challenge)
+          puts "Received challenge response (#{str.length}): #{str.inspect}"
+          need_challenge = !(str.sub(STR_X0, STR_EMPTY) =~ RX_NO_CHALLENGE)
+          if need_challenge
+            str = str.sub(RX_CHALLENGE, STR_EMPTY).gsub(RX_CHALLENGE2, STR_EMPTY).to_i
+            challenge_packet = sprintf(STR_BLA, handle_chr(str >> 24), handle_chr(str >> 16), handle_chr(str >> 8), handle_chr(str >> 0))
+            entry[:needs_challenge] = challenge_packet
+          end
+
+          entry[:state] = 2
+        when 3, 4
+          begin
+            #data = s.recvfrom(RECEIVE_SIZE)
+            data = s.recvfrom_nonblock(RECEIVE_SIZE)
+          rescue => e
+            puts "Error: #{e.message}, #{entry}"
+            entry[:failed] = true
+            sockets.delete s
+            s.close
+            return
+          end
+          puts "Read (3,4): #{s.inspect}, #{entry}: #{data}"
+          entry[:stamp] = Time.now
+          entry[:state] = 4
+
+          index = 0
+
+          game_data = get_string(data[0])
+          Tools.debug {"Received (#{entry[:data].size + 1}):\n\n#{game_data.inspect}\n\n#{game_data}\n\n"}
+
+          if game_data.sub(STR_GARBAGE, STR_EMPTY)[RX_SPLITNUM]
+            splitnum = $1
+            flag = splitnum.unpack("C")[0]
+            index = (flag & 127).to_i
+            last = flag & 0x80 > 0
+            # Data could be received out of order, use the "index" id when "last" flag is true, to determine total packet_count
+            entry[:max_packets] = index + 1 if last # update the max
+            puts "Splitnum: #{splitnum.inspect} (#{splitnum}) (#{flag}, #{index}, #{last}) Max: #{entry[:max_packets]}"
+          else
+            entry[:max_packets] = 1
+          end
+          entry[:data][index] = game_data #.sub(RX_X0_S, STR_EMPTY) # Cut off first \x00 from package
+
+          if entry[:data].size >= entry[:max_packets] # OR we received the end-packet and all packets required
+            puts "Received packet limit: #{entry}"
+            entry[:state] = 5
+            sockets.delete s
+            s.close unless s.closed?
+          end
+      end
+    end
+
+    def handle_write s, entry, sockets
+      #puts "Write: #{s.inspect}, #{entry}"
+      begin
+        case entry[:state]
+          when 0
+            puts "Write (0): #{entry}"
+            # Send Challenge
+            s.puts @packet
+            entry[:state] = 1
+            entry[:stamp] = Time.now
+          when 2
+            puts "Write (2): #{entry}"
+            # Send Challenge response
+            packet = entry[:needs_challenge] ? BASE_PACKET + @id_packet + entry[:needs_challenge] + FULL_INFO_PACKET_MP : BASE_PACKET + @id_packet + FULL_INFO_PACKET_MP
+            s.puts packet
+
+            entry[:state] = 3
+            entry[:stamp] = Time.now
+        end
+      rescue => e
+        puts "Error: #{e.message}, #{entry}"
+        entry[:failed] = true
+        sockets.delete s
+        s.close
+        return
+      end
+
+      if Time.now - entry[:stamp] > @timeout
+        puts "TimedOut: #{entry}"
+        entry[:failed] = true
+        sockets.delete s
+        s.close unless s.closed?
+      end
+    end
+
+    def handle_exc s, entry, sockets
+      #  puts "Exception: #{s.inspect}"
+      #  sockets.delete s
+      #  s.close
+      #  entry = jar[s]
+      #  entry[:failed] = true
     end
   end
 end
@@ -173,11 +212,11 @@ if $0 == __FILE__
   srv = File.open("servers.txt") { |f| f.read }
   addrs = []
   srv.each_line do |line|
-    p line
     addrs << "#{$1}:#{$2}" if line =~ /([\d\.]+)[\s\t]*(\d+)/
   end
   # addrs = ["192.168.50.1:2356", "89.169.242.67:2302"]
-  #addrs = ["24.3.36.214:2311"]
-  sm = GamespyQuery::SocketMaster.new(addrs)
+  #addrs = ["95.156.228.83:2402"]
+  #addrs = addrs[0..9]
+  sm = GamespyQuery::SocketMaster.new(addrs.reverse)
   sm.process!
 end
