74ac092a9c9164fb73a71a0d0579b4d1102592e6
Sickboy <sb@dev-heaven.net>
2012-03-04 12:16:52 +0100

    Prepared Base test

---
+++ test/units/base_test.rb
@@ -0,0 +1,16 @@
+require 'teststrap'
+
+context "Base" do
+  setup { GamespyQuery::Base.new }
+
+end
+
+context "Funcs" do
+  setup { GamespyQuery::Funcs }
+
+end
+
+context "Tools" do
+  setup { GamespyQuery::Tools }
+
+end
