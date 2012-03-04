137b3c275319fdf172ddbd4233dd3fbd8588f2b9
Sickboy <sb@dev-heaven.net>
2012-03-04 14:38:39 +0100

    Added missing default MasterOptions tests

--- test/units/options_test.rb
+++ test/units/options_test.rb
@@ -25,4 +25,18 @@ end
 context "MasterOptions" do
   setup { GamespyQuery::MasterOptions }
 
+  context "Default params" do
+    setup { topic.parse }
+    asserts("returns an OpenStruct") { topic.is_a?(OpenStruct) }
+    asserts("tasks is Array") { topic.tasks.is_a?(Array) }
+  end
+
+  context "Specific params" do
+    asserts("verbose") { topic.parse(["-v"]).verbose }.equals true
+    asserts("no-verbose") { topic.parse(["--no-verbose"]).verbose }.equals false
+
+    # TODO: How to test the exit properly?
+    asserts("version") { mock(GamespyQuery::MasterOptions).exit { 0 }; topic.parse(["--version"])} #.equals 0
+    asserts("help") { mock(GamespyQuery::MasterOptions).exit { 0 }; topic.parse(["--help"]) } #.equals 0
+  end
 end
