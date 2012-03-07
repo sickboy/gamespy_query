fb968125c2ae7eededd365fe85aec3353a7d8f38
Sickboy <sb@dev-heaven.net>
2012-03-07 19:40:59 +0100

    Fixed Options test

--- test/units/options_test.rb
+++ test/units/options_test.rb
@@ -1,45 +1,27 @@
 require 'teststrap'
 require 'ostruct'
 
-=begin
 context "Options" do
   context "Parser" do
     setup { GamespyQuery::Options }
 
-    context "Default params" do
-      setup { topic.parse }
-      asserts("returns an OpenStruct") { topic.is_a?(OpenStruct) }
-      asserts("tasks is Array") { topic.tasks.is_a?(Array) }
-    end
+    asserts("Default Params") { topic.parse([]) }.raises SystemExit
 
-    context "Specific params" do
-      #asserts("no-verbose") { topic.parse([]).options.verbose? }.equals false
-      asserts("verbose") { topic.parse(["-v"]).options.verbose? }.equals true
-      asserts("empty argv") { topic.parse(["-v"]).argv }.same_elements []
+    asserts("version") { topic.parse(["--version"]) }.raises SystemExit #.equals 0
+    asserts("help") { topic.parse(["--help"]) }.raises SystemExit #.equals 0
+                                                                        #asserts("no-verbose") { topic.parse([]).options.verbose? }.equals false
+    asserts("verbose") { topic.parse(["-v"]) }.raises SystemExit
 
-      # TODO: How to test the exit properly?
-      asserts("version") { topic.parse(["--version"]) } #.equals 0
-      asserts("help") { topic.parse(["--help"]) } #.equals 0
-
-      asserts("sync tasks") { topic.parse(["sync", "127.0.0.1:2302"]).tasks }.same_elements [:sync]
-      asserts("sync argv") { topic.parse(["sync", "127.0.0.1:2302"]).argv }.same_elements ["127.0.0.1:2302"]
+    context "Commands" do
+      asserts("sync") { topic.parse(["sync", "127.0.0.1:2302"]) }.nil
     end
 
     context "Master" do
-      #asserts("no-verbose") { topic.parse([]).options.verbose? }.equals false
-      asserts("verbose") { topic.parse(["master", "-v"]).options.verbose? }.equals true
-      asserts("empty argv") { topic.parse(["master", "-v"]).argv }.same_elements []
-
-      # TODO: How to test the exit properly?
-      asserts("version") { topic.parse(["master", "--version"])} #.equals 0
-      asserts("help") { topic.parse(["master", "--help"]) } #.equals 0
-
-      asserts("list") { topic.parse(["master", "list"]).tasks }.same_elements [:list]
-
-      asserts("process") { topic.parse(["master", "process"]).tasks }.same_elements [:process]
-      asserts("process_master") { topic.parse(["master", "process_master"]).tasks }.same_elements [:process_master]
+      context "Commands" do
+        asserts("list") { any_instance_of(GamespyQuery::Master) { |c| mock(c).read { "Test data" } }; topic.parse(["master", "list"]) }.nil
+        asserts("process") { any_instance_of(GamespyQuery::Master) { |c| mock(c).process { "Test data" } }; topic.parse(["master", "process"]) }.nil
+        asserts("process_master") { mock(GamespyQuery::SocketMaster).process_master("arma2oapc", "") { "Test data" }; topic.parse(["master", "process_master"]) }.nil
+      end
     end
-
   end
 end
-=end
